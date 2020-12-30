a310216e8d8d94fc10d74af6f52fa6900a571860
Victor Hyde <victor_hyde@outlook.com>
2020-12-29 22:18:59 -0500

    Delete config.py

--- backend/PythonCode/config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#This File should be in the .gitignore as to not leak the api key once the project is complete
-
-riot_games_api_key = "RGAPI-3a7ceeb4-430f-41a4-9a7b-b1149d3e495b"
-
